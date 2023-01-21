2675b17fb2d788ef038a3ff303a9978fc352c503
dmitriy-lar <larionov.dm2002@gmail.com>
2023-01-21 21:38:56 +0300

    override base user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, User)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email=email, password=password, **extra_fields)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class UserModel(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return f'self.username - {self.email}' if self.username else self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
